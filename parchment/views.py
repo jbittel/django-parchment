3f57221af38a25dceb9d1024c225481ec2f49328
Jason Bittel <jason.bittel@gmail.com>
2014-08-28 08:22:11 -0700

    Encrypt all provided GET parameters

--- parchment/views.py
+++ parchment/views.py
@@ -1,3 +1,5 @@
+from urllib import urlencode
+
 from django.conf import settings
 from django.views.generic import FormView
 
@@ -8,9 +10,15 @@ from .forms import ParchmentForm
 class ParchmentView(FormView):
     form_class = ParchmentForm
     template_name = 'parchment/login.html'
+    connect_variables = {}
+
+    def get(self, request, *args, **kwargs):
+        for k, v in request.GET.items():
+            self.connect_variables[k] = v
+        return super(ParchmentView, self).get(request, *args, **kwargs)
 
     def get_initial(self):
         sso_key = getattr(settings, 'PARCHMENT_SSO_KEY')
         p = Parchment(sso_key)
-        return {'parch5': p.encrypt('test string'),
+        return {'parch5': p.encrypt(urlencode(self.connect_variables)),
                 'parchiv': p.iv}
