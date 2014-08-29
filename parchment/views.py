fc0dc5c71af3c96c863adf0a2698708e932f1684
Jason Bittel <jason.bittel@gmail.com>
2014-08-29 10:47:42 -0700

    Validate required connect variables

--- parchment/views.py
+++ parchment/views.py
@@ -1,6 +1,7 @@
 from urllib import urlencode
 
 from django.conf import settings
+from django.http import HttpResponseBadRequest
 from django.views.generic import FormView
 
 from .crypto import Parchment
@@ -11,10 +12,13 @@ class ParchmentView(FormView):
     form_class = ParchmentForm
     template_name = 'parchment/login.html'
     connect_variables = {}
+    required_variables = ('student_id', 'customers_firstname', 'customers_lastname')
 
     def get(self, request, *args, **kwargs):
-        for k, v in request.GET.items():
-            self.connect_variables[k] = v
+        for var in self.required_variables:
+            if var not in request.GET:
+                return HttpResponseBadRequest("%s is required" % var)
+        self.connect_variables = request.GET.dict()
         return super(ParchmentView, self).get(request, *args, **kwargs)
 
     def get_initial(self):
