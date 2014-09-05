574ca55073e67f17588cfdc0d1680b2910a11b9e
Jason Bittel <jason.bittel@gmail.com>
2014-09-04 22:55:25 -0700

    Add debug hidden field to form

--- parchment/forms.py
+++ parchment/forms.py
@@ -1,6 +1,12 @@
 from django import forms
+from django.conf import settings
 
 
 class ParchmentForm(forms.Form):
     parch5 = forms.CharField(widget=forms.HiddenInput)
     parchiv = forms.CharField(widget=forms.HiddenInput)
+
+    def __init__(self, *args, **kwargs):
+        super(ParchmentForm, self).__init__(*args, **kwargs)
+        if getattr(settings, 'PARCHMENT_DEBUG_MODE', False):
+            self.fields['debug'] = forms.CharField(widget=forms.HiddenInput)
--- parchment/views.py
+++ parchment/views.py
@@ -21,15 +21,18 @@ class ParchmentView(FormView):
     required_variables = ('student_id', 'customers_firstname', 'customers_lastname')
 
     def get(self, request, *args, **kwargs):
+        self.build_connect_string(request)
+        return super(ParchmentView, self).get(request, *args, **kwargs)
+
+    def build_connect_string(self, request):
         for var in self.required_variables:
             if var not in request.GET:
                 return HttpResponseBadRequest("%s is required" % var)
         self.connect_variables = request.GET.dict()
-        return super(ParchmentView, self).get(request, *args, **kwargs)
-
-    def get_connect_string(self):
         self.connect_variables['ts'] = now().isoformat()
         self.connect_variables['rand'] = uuid.uuid4()
+
+    def encode_connect_string(self):
         return urlencode(self.connect_variables)
 
     def get_initial(self):
@@ -39,8 +42,9 @@ class ParchmentView(FormView):
                 'PARCHMENT_SSO_KEY must be configured with your provided '
                 'SSO key')
         p = Parchment(sso_key)
-        return {'parch5': p.encrypt(self.get_connect_string()),
-                'parchiv': p.iv}
+        return {'parch5': p.encrypt(self.encode_connect_string()),
+                'parchiv': p.iv,
+                'debug': self.connect_variables['rand']}
 
     def get_context_data(self, **kwargs):
         context = super(ParchmentView, self).get_context_data(**kwargs)
