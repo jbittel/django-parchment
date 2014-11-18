98b58444eedb496f189a20ab39797531795c7110
Jason Bittel <jason.bittel@gmail.com>
2014-11-17 22:24:06 -0800

    Rename configuration module

--- parchment/views.py
+++ parchment/views.py
@@ -6,9 +6,9 @@ from django.http import HttpResponseBadRequest
 from django.utils.timezone import now
 from django.views.generic import FormView
 
+from .config import url
 from .crypto import Parchment
 from .forms import ParchmentForm
-from .settings import url
 from .utils import add_query_params
 
 
